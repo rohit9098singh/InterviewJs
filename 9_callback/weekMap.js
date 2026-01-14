72f71c3c973663bfd2c54b14d94383ccfe8c9613
rohit@ROHIT <rohitsingh95724@gmail.com>
2026-01-14 20:21:46 +0530

    feat:added symbol and weakmap

---
+++ 9_callback/weekMap.js
@@ -0,0 +1,30 @@
+// What is a WeakMap?
+// => A WeakMap is a collection of key–value pairs where:
+
+// 1) Keys must be objects
+// 2) Values can be anything
+// 3) Keys are weakly referenced
+
+
+const user={name:"Rohit"}
+const wm = new WeakMap();
+wm.set(user,"logged-in")
+console.log(wm)
+
+console.log(wm.set(user))
+
+
+// What is a WeakSet?
+// ==>A WeakSet is a collection of objects only, stored weakly.
+
+const obj1 = {};
+const obj2 = {};
+
+const ws = new WeakSet();
+ws.add(obj1);
+ws.add(obj2);
+
+console.log(ws.has(obj1)); // true
+
+// obj1 = null → garbage collected automatically
+
